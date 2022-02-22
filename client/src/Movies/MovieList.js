a1e53341cdacf6d39a74246a1b18af33b61b6b03
Mike Ferrete <michael.ferrete@gmail.com>
2022-02-21 21:11:54 -0900

    completed MVP tasks

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function MovieList(props) {
+  const { url, path } = useRouteMatch();
+
   return (
     <div className="movie-list">
+      {/* {console.log(url, path)} */}
       {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
@@ -12,16 +16,20 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  const { url, path } = useRouteMatch();
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+    <Link to={`/movies/${props.movie.id}`}>
+      {/* {console.log(url, path)} */}
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    </Link>
   );
 }
